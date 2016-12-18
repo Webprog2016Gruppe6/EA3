9d31afce32a43c38cf635fe9e8737b8f80b51077
r <raffaela.walcher@chello.at>
2016-12-18 15:49:10 +0100

    Indexed Db geändert Lightbox js von Lokesh Dakar hinzugefügt

--- semp/javascript/javaScript.js
+++ semp/javascript/javaScript.js
@@ -1,7 +1,8 @@
+
 /*hier kommt JavaScriptCode*/
 
 /*Drag and Drop
-* http://www.w3schools.com/html/html5_draganddrop.asp*/
+ * http://www.w3schools.com/html/html5_draganddrop.asp*/
 function droppenErlauben(event) {
     event.preventDefault();
 
@@ -9,59 +10,59 @@ function droppenErlauben(event) {
 
 function draggenInnerTable(event) {
     event.dataTransfer.setData("text", event.target.id);
+    event.dataTransfer.effectAllowed ='copy';
 
 }
 
 function droppenInnerTable(event)  {
     event.preventDefault();
     var data = event.dataTransfer.getData("text");
+    //$('<div>').text(val).appendTo(event.target);
     event.target.appendChild(document.getElementById(data));
-
-
+    event.dataTransfer.dropEffect ='copy';
 }
+
 
 /*Indexed DB*/
 
 
-// This works on all devices/browsers, and uses IndexedDBShim as a final fallback
+//  gewährt Interbrowserkompabilität
 var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
 
-// Open (or create) the database
-var open = indexedDB.open("Stundenplaner", 1);
+// Öffnen der Datenbank
+var request = indexedDB.open("Stundenplaner");
 
-// Create the schema
-open.onupgradeneeded = function() {
-    var db = open.result;
-    var store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
+// Datenbankindex wird erstellt
+request.onupgradeneeded = function() {
+    var database = request.result;
+    var store = database.createObjectStore("MyObjectStore", {keyPath: "id"});
     var indexStundenplan = store.createIndex("StundenplanIndex", ["Stundenplanname",
         "Faecher", "Tage", "Professoren" ]);
     /*Es wird ein Index für die einzelnen Stundenpläne angelegt. Pro angelegtem Stundenplan gibt es
-    * den Stundenplanername, die abgespeicherten Fächer und deren Aufteilung auf die Tage, die
-    * abgespeicherten Professoren zu den einzelnen Fächer.*/
+     * den Stundenplanername, die abgespeicherten Fächer und deren Aufteilung auf die Tage, die
+     * abgespeicherten Professoren zu den einzelnen Fächer.*/
 };
-//Datenbank wird geöffnet
-open.onsuccess = function() {
+//Datenbank wird u.a. beschrieben
+request.onsuccess = function() {
 
-    var db = open.result;
-    var tx = db.transaction("MyObjectStore", "readwrite");
-    var store = tx.objectStore("MyObjectStore");
+    var database = request.result;
+    var transaction = database.transaction("MyObjectStore", "readwrite");
+    var store = transaction.objectStore("MyObjectStore");
     var indexStundenplan = store.index("StundenplanIndex");
 
 
-
-    store.put({id: 10, Stundenplanname: "MeinStundenplan", Tage:"Samstag"});
-
+    store.put({id: 10, Stundenplanname: "MeinStundenplan", Tage: "Samstag"});
 
 
     var getStundenplan = store.get(10);
 
 
-    getStundenplan.onsuccess = function() {
+    getStundenplan.onsuccess = function () {
         alert(getStundenplan.result.Tage);
     };
 
     // Datenbank wird geschlossen
-    tx.oncomplete = function() {
-        db.close();
+    transaction.oncomplete = function () {
+        database.close();
     };
 }

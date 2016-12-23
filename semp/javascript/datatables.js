ca6a7e9e8b743d13ecd3b794ffe6dd57843998ef
r <raffaela.walcher@chello.at>
2016-12-23 03:08:03 +0100

    Layout an Wireframe angepasst aktualisiertes JavaScript an Abgabe EA3

--- semp/javascript/datatables.js
+++ semp/javascript/datatables.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
                 "targets": [ 6 ],
                 "visible": false, //Spalte Samstag wird ausgeblendet
                 "searchable": false
-            },
+            }/*
             {
-                "targets": [7],
+                "targets": [ 7 ],
                 "visible": false, //Spalte Sonntag wird ausgeblendet
                 "searchable": false
-            }
+            }*/
         ],
-        "scrollY": 200,  // legt die Größe des scrollbaren Bereichs fest
-        "scrollX": true, //Hier wird der Stundenplan scrollbar gemacht
+        "scrollY": 100,  // legt die Größe des scrollbaren Bereichs fest
+        "scrollX": 800, //Hier wird der Stundenplan scrollbar gemacht
         "filter": false, //Filter werden ausgeblendet
         "info": false,   //Titel wird ausgeblendet
         "paging": false,

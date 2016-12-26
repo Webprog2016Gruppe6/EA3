24d062aac6d1c21a05e4dbc3c79af58c2a3ed10f
r <raffaela.walcher@chello.at>
2016-12-26 22:44:28 +0100

    window.print() hinzugefügt window.hilfe() hinzugefügt Layoutanpassungen lightbox.js & .css gelöscht offline jquery gelöscht datatables geändert aber auskommentiert, da Endergebnis nicht Wireframe entspricht print.css für Drucken hinzugefügt mobile Weiterleitung via JAvascript im Html hinzugefügt

--- semp/javascript/datatables.js
+++ semp/javascript/datatables.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
                 "targets": [ 6 ],
                 "visible": false, //Spalte Samstag wird ausgeblendet
                 "searchable": false
-            }/*
+            },
             {
                 "targets": [ 7 ],
                 "visible": false, //Spalte Sonntag wird ausgeblendet
                 "searchable": false
-            }*/
+            }
         ],
-        "scrollY": 100,  // legt die Größe des scrollbaren Bereichs fest
-        "scrollX": 800, //Hier wird der Stundenplan scrollbar gemacht
+        /*"scrollY": 100,  // legt die Größe des scrollbaren Bereichs fest
+        "scrollX": true, //Hier wird der Stundenplan scrollbar gemacht*/
         "filter": false, //Filter werden ausgeblendet
         "info": false,   //Titel wird ausgeblendet
         "paging": false,
         //Seitenzahlen werden ausgeblendet, falls Tabelle auf mehrere Seiten aufgeteilt wird
 
     } )
-} )
+})
